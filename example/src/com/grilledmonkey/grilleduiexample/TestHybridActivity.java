e9995313f1141359e3b8c688f007cdd5fd8bf248
Alexander 'Aux' Ivanov <aux@hexmode.org>
2013-06-11 10:23:05 +0300

    Added example got HybridActivity

---
+++ example/src/com/grilledmonkey/grilleduiexample/TestHybridActivity.java
@@ -0,0 +1,25 @@
+package com.grilledmonkey.grilleduiexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import com.grilledmonkey.grilledui.HybridActivity;
+import com.grilledmonkey.grilledui.adapters.SectionAdapter;
+
+public class TestHybridActivity extends HybridActivity {
+	@Override
+	public SectionAdapter createSectionAdapter(FragmentManager fm) {
+		SectionAdapter adapter = new SectionAdapter(fm);
+
+		adapter.add(new TabModFragment(), "Tab actions");
+
+		Fragment fragment = new DummySectionFragment();
+		Bundle args = new Bundle();
+		args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, 888);
+		fragment.setArguments(args);
+		adapter.add(fragment, "888");
+
+		return(adapter);
+	}
+}
