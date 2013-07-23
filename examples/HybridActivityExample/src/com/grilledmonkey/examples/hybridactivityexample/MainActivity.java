1ba9220728818ae02d3171d17b71a00501f2cb90
Alexander 'Aux' Ivanov <aux@hexmode.org>
2013-07-23 13:56:19 +0300

    Added HybridActivity example

--- examples/HybridActivityExample/src/com/grilledmonkey/examples/hybridactivityexample/MainActivity.java
+++ examples/HybridActivityExample/src/com/grilledmonkey/examples/hybridactivityexample/MainActivity.java
@@ -1,12 +1,14 @@
-package com.grilledmonkey.examples.sectionactivityexample;
+package com.grilledmonkey.examples.hybridactivityexample;
 
+import android.app.ActionBar;
+import android.app.ActionBar.TabListener;
 import android.support.v4.app.FragmentManager;
 
-import com.grilledmonkey.grilledui.SectionActivity;
+import com.grilledmonkey.grilledui.HybridActivity;
 import com.grilledmonkey.grilledui.adapters.SectionAdapter;
 
 /**
- * This example shows the basic usage of {@link SectionActivity} with GrilledUI library
+ * This example shows the basic usage of {@link HybridActivity} with GrilledUI library
  * referencing and using provided layouts. GrilledUI layouts can only be used
  * when referencing GrilledUI in source form. Built-in layouts can not be used
  * with pre-compiled library in JAR form.
@@ -20,11 +22,13 @@ import com.grilledmonkey.grilledui.adapters.SectionAdapter;
  * @author Aux
  *
  */
-public class MainActivity extends SectionActivity {
+public class MainActivity extends HybridActivity {
 
 	/**
 	 * Default SectionAdapter is empty, meaning without any tabs. Override
-	 * {@code createSectionAdapter()} method to populate sections.
+	 * {@code createSectionAdapter()} method to populate tabs. Remember to pass
+	 * {@link ActionBar} and {@link TabListener} to {@link SectionAdapter}
+	 * constructor.
 	 * <p/>
 	 * P.S. You may use method chaining, because {@code add()} returns current
 	 * SectionAdapter instance.
@@ -33,7 +37,7 @@ public class MainActivity extends SectionActivity {
 	public SectionAdapter createSectionAdapter(FragmentManager fm) {
 		// Create new SectionAdapter and make current
 		// activity a handler to all events.
-		SectionAdapter adapter = new SectionAdapter(fm);
+		SectionAdapter adapter = new SectionAdapter(fm, getActionBar(), this);
 
 		// Add two sections.
 		adapter.add(new DummySectionFragment(), "First section");
